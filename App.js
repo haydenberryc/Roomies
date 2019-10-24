ba28aed39d8e510427865262fe20bb55fb92ccf7
haydenberryc <haydenberryc@Haydens-MacBook-Pro.local>
2019-10-24 10:07:47 -0500

    [-changed event loader to LoadingScreen]

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { firebaseConfig } from './config';
 
 import LoginScreen from './screens/LoginScreen';
 import DashboardScreen from './screens/DashboardScreen';
@@ -10,17 +11,6 @@ import CalendarScreen from './screens/CalendarScreen';
 import EventFormScreen from './screens/EventFormScreen';
 
 import * as firebase from 'firebase';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCeE9R9VgRC0PLFcrTB4o0bwjfodDcASy4",
-  authDomain: "roommatescheduler.firebaseapp.com",
-  databaseURL: "https://roommatescheduler.firebaseio.com",
-  projectId: "roommatescheduler",
-  storageBucket: "roommatescheduler.appspot.com",
-  messagingSenderId: "85029936204",
-  appId: "1:85029936204:web:c5f09800e848b9a47f28e9",
-  measurementId: "G-DXGKYE5TX9"
-};
 
 firebase.initializeApp(firebaseConfig);
 
